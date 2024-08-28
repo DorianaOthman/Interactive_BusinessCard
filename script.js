12c9c5920513e6420714ab96423aa21bcaf0a2f6
Doriana Othman <doriothman@gmail.com>
2024-08-28 14:51:02 -0600

    Fixed Flipping
    Links now do not flip card when pressed. Cleaned up code to be neater

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.getElementById('business-card').addEventListener('click', function() {
+/*The code below allows the buisness card to flip *only* 
+when the card itself is clicked, not any links */
+
+document.getElementById('business-card').addEventListener('click', function(event) {
+    if (event.target.closest('.no-flip')) {
+        return;
+    }
     this.classList.toggle('flipped');
 });
